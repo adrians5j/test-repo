b40971d7b2fba6c7f403f1b1063027f5525cd4b9
Adrian Smijulj <adrian1358@gmail.com>
2021-11-18 17:45:22 +0100

    test

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 #!/usr/bin/env node
-const args = require('yargs')
 
-console.log(args)
-console.log("I was executed from a gist inside the terminal with npx!");
+const fs = require("fs");
+const path = require("path");
+
+const response = data => {
+    console.log(JSON.stringify(data));
+    process.exit(0);
+};
+
+try {
+    const { argv } = require("yargs");
+
+    const [script, callback] = argv._;
+    const scriptsPath = fs.existsSync(path.join(__dirname, script, "index.js"));
+    if (!fs.existsSync(scriptsPath)) {
+        response({
+            type: "error",
+            message: "Script does not exist.",
+            code: "SCRIPT_DOES_NOT_EXIST"
+        });
+    }
+
+    const scripts = require(scriptsPath);
+    if (!scripts[callback]) {
+        response({
+            type: "error",
+            message: `Script does exist, but "${callback}" callback is not exported.`,
+            code: "SCRIPT_DOES_NOT_EXIST"
+        });
+    }
+
+    response(script[callback]);
+} catch (e) {
+    console.log({ type: "error", message: e.message, code: "ERROR" });
+}
