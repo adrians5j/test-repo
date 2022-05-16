a17f223ad6ac0e582adab8370d6142d57cabbc7f
Adrian Smijulj <adrian1358@gmail.com>
2022-05-16 10:19:22 +0200

    test

--- testing-pushes-comits.js
+++ testing-pushes-comits.js
@@ -1,18 +1,30 @@
 const execa = require("execa");
 
 (async () => {
+    const ts = new Date().getTime();
+
     {
         const result = execa.sync("git", ["fetch"]);
         console.log(result.stdout);
     }
 
     {
-        const result = execa.sync("git", [
-            "checkout",
-            "-b",
-            `webiny-${new Date().getTime()}`,
-            "origin/master"
-        ]);
+        const result = execa.sync("git", ["checkout", "-b", `webiny-${ts}`, "origin/master"]);
+        console.log(result.stdout);
+    }
+
+    {
+        const result = execa.sync("touch", [`file-${ts}.txt`]);
+        console.log(result.stdout);
+    }
+
+    {
+        const result = execa.sync("git", ["add", "."]);
+        console.log(result.stdout);
+    }
+
+    {
+        const result = execa.sync("git", ["commit", "-m", `"testing"`]);
         console.log(result.stdout);
     }
 
